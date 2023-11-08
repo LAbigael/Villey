5db082359d455354c18be8bc099b1d5a217b1e7f
Quentin GLX <quentin.glorieux@gmail.com>
2023-11-08 23:32:24 +0100

    first commit UI

--- juspoliticum-ui/astro.config.mjs
+++ juspoliticum-ui/astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: true
-  },
-  integrations: [vue()]
+  integrations: [vue(), tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ["modern-normalize"]
+    }
+  }
 });
--- juspoliticum-ui/src/utils/get-directus-client.js
+++ juspoliticum-ui/src/utils/get-directus-client.js
@@ -1,1 +1,8 @@
-import { createDirectus } from '@directus/sdk';
+import { createDirectus, rest, staticToken} from '@directus/sdk';
+
+export const getDirectusClient = async () => {
+  const directus = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL)
+                      .with(staticToken(import.meta.env.DIRECTUS_STATIC_TOKEN))
+                      .with(rest());
+  return directus;
+};
