8a008452b826643abc634294bdcc6f2ca3a7a798
Abigaël Laldji <ablaldji@gmail.com>
2023-11-09 10:03:44 +0100

    fix: docker compose expose astro port

--- juspoliticum-ui/astro.config.mjs
+++ juspoliticum-ui/astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
+  server: { port: 4321, host: true },
   vite: {
     ssr: {
       noExternal: ["modern-normalize"]
