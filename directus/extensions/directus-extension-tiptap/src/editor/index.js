031fb35cc165924dee50102eedbf5b23e6ed3030
Abigaël Laldji <ablaldji@gmail.com>
2023-11-17 20:54:21 +0100

    feat: add tiptap extension to directus

---
+++ directus/extensions/directus-extension-tiptap/src/editor/index.js
@@ -0,0 +1,11 @@
+import InterfaceComponent from './interface.vue';
+
+export default {
+	id: 'tiptap',
+	name: 'Tiptap',
+	icon: 'box',
+	description: 'WYSIWYG editor for Directus',
+	component: InterfaceComponent,
+	options: null,
+	types: ['json'],
+};
---
+++ directus/extensions/directus-extension-tiptap/src/editor/interface.vue
@@ -0,0 +1,23 @@
+<template>
+	<input :value="value" @input="handleChange($event.target.value)" />
+	<div>coucou</div>
+</template>
+
+<script>
+export default {
+	props: {
+		value: {
+			type: String,
+			default: null,
+		},
+	},
+	emits: ['input'],
+	setup(props, { emit }) {
+		return { handleChange };
+
+		function handleChange(value) {
+			emit('input', value);
+		}
+	},
+};
+</script>
