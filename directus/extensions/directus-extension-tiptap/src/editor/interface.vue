aceafd1f6b699ceec2a292f551250c4c3706a0c6
Abigaël Laldji <ablaldji@gmail.com>
2023-11-23 14:30:48 +0100

    feat directus: add tiptap field

--- directus/extensions/directus-extension-tiptap/src/editor/interface.vue
+++ directus/extensions/directus-extension-tiptap/src/editor/interface.vue
@@ -1,23 +1,103 @@
 <template>
-	<input :value="value" @input="handleChange($event.target.value)" />
-	<div>coucou</div>
+  <button
+    @click="editor.chain().focus().insertContent({ type: 'footnote' }).run()"
+  >
+    insert footnote
+  </button>
+  <editor-content class="content" :editor="editor" />
+  <button @click="save">save</button>
 </template>
 
 <script>
-export default {
-	props: {
-		value: {
-			type: String,
-			default: null,
-		},
-	},
-	emits: ['input'],
-	setup(props, { emit }) {
-		return { handleChange };
+import { useEditor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+import Footnote from "tiptap-extension-footnote";
+import { watch } from "vue";
 
-		function handleChange(value) {
-			emit('input', value);
-		}
-	},
+export default {
+  components: {
+    EditorContent,
+  },
+
+  props: {
+    value: {
+      type: JSON,
+      default: { type: "doc", content: [{ type: "paragraph" }] },
+    },
+  },
+  emits: ["input"],
+  setup(props, { emit }) {
+    console.log(JSON.stringify(props));
+    const editor = useEditor({
+      extensions: [StarterKit, Footnote],
+      onUpdate: ({ editor }) => {
+        console.log("update");
+        emit("input", editor.getJSON());
+      },
+    });
+
+    const unwatch = watch(
+      () => props.value,
+      (value) => {
+        if (value) {
+          editor.value?.commands.setContent(value);
+          console.log(unwatch);
+
+          unwatch();
+        }
+      }
+    );
+
+    return { editor };
+  },
 };
 </script>
+
+<style>
+.ProseMirror {
+  font-size: large;
+  counter-reset: footnote;
+}
+
+footnote {
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+}
+
+footnote::after {
+  content: counter(footnote);
+  vertical-align: super;
+  font-size: 75%;
+  counter-increment: footnote;
+}
+.ProseMirror-hideselection .footnote-tooltip *::selection {
+  background-color: transparent;
+}
+.ProseMirror-hideselection .footnote-tooltip *::-moz-selection {
+  background-color: transparent;
+}
+.footnote-tooltip {
+  color: #333;
+  cursor: auto;
+  position: absolute;
+  left: -30px;
+  top: calc(100% + 10px);
+  background: silver;
+  padding: 3px;
+  border-radius: 2px;
+  width: 500px;
+}
+.footnote-tooltip::before {
+  border: 5px solid silver;
+  border-top-width: 0px;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  position: absolute;
+  top: -5px;
+  left: 27px;
+  content: " ";
+  height: 0;
+  width: 0;
+}
+</style>
