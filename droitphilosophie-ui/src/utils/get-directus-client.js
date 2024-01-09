ae3325e1c0a140b90e07f7495a2a4914d5f1d0cd
Abigaël Laldji <ablaldji@gmail.com>
2024-01-09 15:28:42 +0100

    increase max width of content

--- droitphilosophie-ui/src/utils/get-directus-client.js
+++ droitphilosophie-ui/src/utils/get-directus-client.js
@@ -1,12 +1,13 @@
-import { createDirectus, rest, staticToken} from '@directus/sdk';
+import { createDirectus, rest, staticToken, readItems } from '@directus/sdk';
 
 export const getDirectusClient = async () => {
   const directus = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL)
-                      .with(staticToken(import.meta.env.DIRECTUS_STATIC_TOKEN))
-                      .with(rest());
+    .with(staticToken(import.meta.env.DIRECTUS_STATIC_TOKEN))
+    .with(rest());
   return directus;
 };
 
 export const getImagePath = (image) => {
   return `http://localhost:8055/assets/${image}`;
 };
+
