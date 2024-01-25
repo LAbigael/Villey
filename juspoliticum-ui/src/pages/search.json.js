cb191baeb0bbb0fa2181253d49d631b7a27dc34a
Abigaël Laldji <ablaldji@gmail.com>
2024-01-25 11:09:12 +0100

    reactor search

--- juspoliticum-ui/src/pages/search.json.js
+++ juspoliticum-ui/src/pages/search.json.js
@@ -4,16 +4,6 @@ import { readItems } from "@directus/sdk";
 const directus = await getDirectusClient(true);
 
 async function getData() {
-  // const volumes = await directus.request(
-  //   readItems("Volumes", {
-  //     fields: ["slug", "title"],
-  //     filter: {
-  //       site_id: {
-  //         _eq: "2",
-  //       },
-  //     },
-  //   }),
-  // );
   const articles = await directus.request(
     readItems("Articles", {
       fields: [
@@ -34,53 +24,16 @@ async function getData() {
       },
     }),
   );
-  // const themes = await directus.request(
-  //   readItems("Themes", {
-  //     fields: ["slug", "name"],
-  //     filter: {
-  //       site_id: {
-  //         _eq: "2",
-  //       },
-  //     },
-  //   }),
-  // );
-  // const authors = await directus.request(
-  //   readItems("Authors", {
-  //     fields: ["slug", "fullname"],
-  //     filter: {
-  //       site_id: {
-  //         _eq: "2",
-  //       },
-  //     },
-  //   }),
-  // );
 
   return [
-    // ...volumes.map((volume) => ({
-    //   slug: volume.slug,
-    //   title: volume.title,
-    //   type: "volume",
-    // })),
     ...articles.map((article) => ({
       slug: article.slug,
       title: article.title,
-      section: article.section_id?.title,
-      volume: article.section_id?.volume_id?.title,
+      volumeTitle: article.section_id?.volume_id?.title,
       volumeNb: article.section_id?.volume_id?.number,
       authors: article.authors?.map((author) => author.author_id.fullname),
-      type: "article",
       themes: article.themes?.map((theme) => theme.theme_id.name),
     })),
-    // ...themes.map((theme) => ({
-    //   slug: theme.slug,
-    //   title: theme.name,
-    //   type: "theme",
-    // })),
-    // ...authors.map((author) => ({
-    //   slug: author.slug,
-    //   title: author.fullname,
-    //   type: "author",
-    // })),
   ];
 }
 
