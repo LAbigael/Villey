1fbf59a27cda44aa1e8c75d6e8c98b7d969b6a53
Abigaël Laldji <ablaldji@gmail.com>
2024-02-15 10:36:20 +0100

    convert docx w to tiptap

--- directus/extensions/directus-extension-tiptap/src/convert-document-to-tiptap/index.js
+++ directus/extensions/directus-extension-tiptap/src/convert-document-to-tiptap/index.js
@@ -1,38 +1,82 @@
 import Busboy from "busboy";
+import mammoth from "mammoth";
+import os from "os";
+import fs from "fs";
+import path from "path";
+import {
+  modifyJsonToMatchTiptapSchema,
+  transformHTML,
+  htmlToTiptap,
+  extractFootnotes,
+} from "./service";
+
+function transformElement(element) {
+  if (element.children) {
+    var children = element.children.map(transformElement);
+    element = { ...element, children: children };
+  }
+
+  if (element.type === "paragraph") {
+    element = transformParagraph(element);
+  }
+
+  return element;
+}
+
+function transformParagraph(element) {
+  if (element.alignment === "center") {
+    return { ...element, styleName: "center-aligned" };
+  } else if (element.alignment === "right") {
+    return { ...element, styleName: "right-aligned" };
+  } else if (element.alignment === "left") {
+    return { ...element, styleName: "left-aligned" };
+  } else {
+    return element;
+  }
+}
 
 export default {
   id: "convert-document-to-tiptap",
-  handler: (router, context) => {
-    const { services, getSchema } = context;
-    const { FilesService } = services;
-    router.get("/hello", (req, res) => {
-      res.send("Hello World");
-    });
-
+  handler: (router) => {
     router.post("/", async (req, res, next) => {
-      const filesService = new FilesService({
-        schema: await getSchema(),
-        accountability: req.accountability,
-      });
-
       const busboy = Busboy({ headers: req.headers });
 
-      busboy.on("file", async (_, fileStream, { filename, mimeType }) => {
-        const data = {
-          filename_download: filename,
-          type: mimeType,
-          storage: "local",
+      busboy.on("file", (name, file, info) => {
+        const saveTo = path.join(os.tmpdir(), `busboy-upload-${name}`);
+        file.pipe(fs.createWriteStream(saveTo));
+        const options = {
+          styleMap: [
+            "small-caps => span.small-caps",
+            "p[style-name='Quotations'] => blockquote > p:fresh",
+            "p[style-name='left-aligned'] => p.left-aligned:fresh",
+            "p[style-name='center-aligned'] => p.center-aligned:fresh",
+            "p[style-name='right-aligned'] => p.right-aligned:fresh",
+          ],
+          includeEmbeddedStyleMap: true,
+          transformDocument: transformElement,
         };
+        file.on("end", () => {
+          mammoth
+            .convertToHtml({ path: saveTo }, options)
+            .then(function(result) {
+              let { footnotes, html } = extractFootnotes(result.value);
 
-        try {
-          const primaryKey = await filesService.uploadOne(fileStream, data);
+              html = transformHTML(html);
 
-         const deleted =  await filesService.deleteOne(primaryKey);
+              let articleContent = htmlToTiptap(html);
 
-          res.json({ status: "ok" });
-        } catch (error) {
-          busboy.emit("error", error);
-        }
+              articleContent = modifyJsonToMatchTiptapSchema(
+                articleContent,
+                footnotes
+              );
+
+              res.writeHead(200, { "Content-Type": "text/json" });
+              res.end(JSON.stringify(articleContent));
+            })
+            .catch(function(error) {
+              console.error(error);
+            });
+        });
       });
 
       busboy.on("error", (error) => {
---
+++ directus/extensions/directus-extension-tiptap/src/convert-document-to-tiptap/service.js
@@ -0,0 +1,142 @@
+import * as htmlparser2 from "htmlparser2";
+import render from "dom-serializer";
+import { uid } from "uid";
+import { generateJSON } from "@tiptap/html";
+import StarterKit from "@tiptap/starter-kit";
+import Footnote, { FontVariant } from "tiptap-extension-footnote";
+import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
+import TextStyle from "@tiptap/extension-text-style";
+import Table from "@tiptap/extension-table";
+import TableRow from "@tiptap/extension-table-row";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import Image from "@tiptap/extension-image";
+
+export const htmlToTiptap = (html) => {
+  const articleContent = generateJSON(html, [
+    StarterKit,
+    Link,
+    TextAlign.configure({
+      types: ["paragraph", "heading", "blockquote"],
+    }),
+    TextStyle.configure({
+      HTMLAttributes: {
+        style: "font-variant",
+      },
+    }),
+    FontVariant,
+    Footnote,
+    Table,
+    TableRow,
+    TableCell,
+    TableHeader,
+    Image,
+  ]);
+  return articleContent;
+};
+export function transformHTML(html) {
+  html = html.replace(
+    /<span class="small-caps">(.*?)<\/span>/g,
+    "<span style='font-variant: small-caps;'>$1</span>"
+  );
+
+  // <p class="center-aligned">*</p>
+  html = html.replace(
+    /<p class="center-aligned">(.*?)<\/p>/g,
+    "<p style='text-align: center;'>$1</p>"
+  );
+  html = html.replace(
+    /<p class="right-aligned">(.*?)<\/p>/g,
+    "<p style='text-align: right;'>$1</p>"
+  );
+  html = html.replace(
+    /<p class="left-aligned">(.*?)<\/p>/g,
+    "<p style='text-align: left;'>$1</p>"
+  );
+
+  return html;
+}
+export function extractFootnotes(html) {
+  const footnotes = [];
+  const dom = htmlparser2.parseDocument(html);
+  htmlparser2.DomUtils.getElements({ tag_name: "li" }, dom).forEach((el, i) => {
+    let id = el.attribs.id;
+    if (id && id.startsWith("footnote-")) {
+      let content = render(el.children[0]).replace(/<a.*?<\/a>/, "");
+      content = transformHTML(content);
+      content = htmlToTiptap(content);
+      footnotes.push({ position: i + 1, id, content, uid: uid() });
+    }
+  });
+
+  // get ordered list containing element with id like footnote and remove it from html
+  const parentList = htmlparser2.DomUtils.getElements(
+    { tag_name: "ol" },
+    dom
+  ).find((el) => {
+    return el.children.some((child) => {
+      return (
+        child.attribs &&
+        child.attribs.id &&
+        child.attribs.id.startsWith("footnote-")
+      );
+    });
+  });
+  if (parentList) {
+    const parentListIndex = dom.children.indexOf(parentList);
+    dom.children.splice(parentListIndex, 1);
+  }
+
+  return { html: render(dom), footnotes };
+}
+
+export const modifyJsonToMatchTiptapSchema = (json, footnotes = []) => {
+  const replaceWithTiptapMarks = (node) => {
+    if (node.marks) {
+      node.marks.forEach((mark) => {
+        if (footnotes.length > 0) {
+          if (mark.type === "link" && mark.attrs.href) {
+            replaceLinkWithFootnote();
+          }
+        }
+
+        function replaceLinkWithFootnote() {
+          // remove # from href (ex: #footnote-3)
+          
+          const footnoteRef = mark.attrs.href.replace("#", "");
+          if (!footnoteRef) {
+            // normal link
+            return;
+          }
+
+          const footnote = footnotes.find((f) => f.id === footnoteRef);
+          if (footnote) {
+            node.type = "footnote";
+            // remove [1] from text
+            // footnote.content = footnote.content.replace(/\[\d+\]/g, "").trim();
+            node.content = footnote.content
+            node.attrs = {
+              uid: footnote.uid,
+            };
+            delete node.marks;
+            delete node.text;
+          } else {
+            // console.log("footnote not found", footnoteNumber);
+          }
+        }
+      });
+    }
+
+    if (node.content ) {
+      if (!Array.isArray(node.content)) {
+        node.content = [node.content];
+      }
+      node.content.forEach((child) => replaceWithTiptapMarks(child));
+    }
+  };
+
+  json.content.forEach((node) => replaceWithTiptapMarks(node));
+
+  return json
+};
