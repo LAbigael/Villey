91233b00c7b81f3cc771edacfbad715dfd2b6cbb
Quentin GLX <quentin.glorieux@gmail.com>
2024-03-07 11:38:17 +0100

    update Header.astro, Menu.astro, ThemeToggle.astro and tailwind.config.mjs

--- juspoliticum-ui/tailwind.config.mjs
+++ juspoliticum-ui/tailwind.config.mjs
@@ -25,7 +25,7 @@ export default {
         "background-alt": "#ebeae9",
         "background-alt-dark": "#524134",
         "background-light": "#f5f4f3",
-        "background-dark": "#1b140d",
+        "background-dark": "#292524",
         color: "#101010",
         "color-light": "#e8e8e8",
         "accent-gradient-dark":
