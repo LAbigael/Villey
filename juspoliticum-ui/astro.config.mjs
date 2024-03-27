5a453ec6220bde157ac541d168053de7e1c437d7
Quentin GLX <quentin.glorieux@gmail.com>
2024-03-28 00:10:01 +0100

    build(deps): update astro.config.mjs, package-lock.json and package.json

--- juspoliticum-ui/astro.config.mjs
+++ juspoliticum-ui/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 import vue from "@astrojs/vue";
 
 import tailwind from "@astrojs/tailwind";
@@ -8,8 +8,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [vue(), tailwind()],
   server: { port: 4322, host: true },
-  output: 'hybrid',
-  adapter: vercel(),
+  output: 'server',
+  adapter: netlify(),
   vite: {
     ssr: {
       noExternal: ["modern-normalize"]
