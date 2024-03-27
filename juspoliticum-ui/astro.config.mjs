7b0930c72d9d216180b66e419a6ff5365caa6f34
Quentin GLX <quentin.glorieux@gmail.com>
2024-03-28 00:25:20 +0100

    update astro.config.mjs

--- juspoliticum-ui/astro.config.mjs
+++ juspoliticum-ui/astro.config.mjs
@@ -7,9 +7,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  server: { port: 4322, host: true },
-  output: 'server',
-  adapter: netlify(),
+  // server: { port: 4322, host: true },
+  output: 'static',
+  // adapter: netlify(),
   vite: {
     ssr: {
       noExternal: ["modern-normalize"]
