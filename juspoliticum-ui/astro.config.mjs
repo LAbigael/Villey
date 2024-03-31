3de83ac0fdddfb635d77b884f9e9e00acd0bedde
Quentin GLX <quentin.glorieux@gmail.com>
2024-03-31 23:30:07 +0200

    build(deps): delete 1 file and update 3 files

--- juspoliticum-ui/astro.config.mjs
+++ juspoliticum-ui/astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
   // server: { port: 4322, host: true },
   output: 'server',
-  // adapter: netlify(),
   vite: {
     ssr: {
       noExternal: ["modern-normalize"]
     }
-  }
+  },
+  adapter: vercel()
 });
