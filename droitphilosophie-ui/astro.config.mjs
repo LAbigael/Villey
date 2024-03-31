e691207794b0aa70eba967789830a642ffb30e26
Quentin GLX <quentin.glorieux@gmail.com>
2024-03-31 23:40:17 +0200

    update astro.config.mjs and package.json

--- droitphilosophie-ui/astro.config.mjs
+++ droitphilosophie-ui/astro.config.mjs
@@ -1,16 +1,21 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  server: { port: 4321, host: true },
+  server: {
+    port: 4321,
+    host: true
+  },
   output: "server",
   vite: {
     ssr: {
       noExternal: ["modern-normalize"]
     }
-  }
+  },
+  adapter: vercel()
 });
