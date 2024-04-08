a8f636014561903166d23a3eabd47851a9f9a483
Abigael Laldji <ablaldji@gmail.com>
2024-04-08 22:40:54 +0200

    fix lettrine for ebooks

--- droitphilosophie-ui/public/js/add-lettrine.js
+++ droitphilosophie-ui/public/js/add-lettrine.js
@@ -3,49 +3,61 @@
 
 let firstParagraph;
 
-
 if (!document.querySelector(".drop-cap")) {
-for (
-  let i = 0;
-  i < document.querySelector(".article_content").children.length;
-  i++
-) {
-  if (document.querySelector(".article_content").children[i].tagName === "P") {
+  for (
+    let i = 0;
+    i < document.querySelector(".article_content").children.length;
+    i++
+  ) {
     if (
-      document.querySelector(".article_content").children[i].innerHTML.length >
-      0
+      document.querySelector(".article_content").children[i].tagName === "P"
     ) {
-      firstParagraph = document.querySelector(".article_content").children[i];
-      let firstLetter = firstParagraph.innerHTML[0];
-      // if is a new line, then continue to next paragraph
-      if (firstLetter === "&") {
-        continue;
+      if (
+        document.querySelector(".article_content").children[i].innerHTML
+          .length > 0
+      ) {
+        firstParagraph = document.querySelector(".article_content").children[i];
+        let firstLetter = firstParagraph.innerHTML[0];
+
+        // if is a new line, then continue to next paragraph
+        if (firstLetter === "&") {
+          continue;
+        }
+        // if <span style="font-variant">&nbsp; is the first letter, then continue to next paragraph
+        if (firstLetter === "<") {
+          continue;
+        }
+        // if first paragraph is not aligned to left, then continue to next paragraph
+        if (
+          firstParagraph.style.textAlign &&
+          firstParagraph.style.textAlign !== "left"
+        ) {
+          firstParagraph = null;
+          continue;
+        }
+
+        break;
       }
-      // if <span style="font-variant">&nbsp; is the first letter, then continue to next paragraph
-      if (firstLetter === "<") {
-        continue;
-      }
-      
-      break;
     }
   }
-}
 
-//if begin with «, make the two first letters lettrine
-// extract first letter and wrap it in span and also apostrophe if there is one
-let firstLetter = firstParagraph.innerHTML[0];
-if (firstLetter === "«") {
-  firstLetter += firstParagraph.innerHTML[1] + firstParagraph.innerHTML[2];
-  firstParagraph.innerHTML = firstParagraph.innerHTML.slice(2);
-}
+  //if begin with «, make the two first letters lettrine
+  // extract first letter and wrap it in span and also apostrophe if there is one
+  if (firstParagraph) {
+    let firstLetter = firstParagraph.innerHTML[0];
+    if (firstLetter === "«") {
+      firstLetter += firstParagraph.innerHTML[1] + firstParagraph.innerHTML[2];
+      firstParagraph.innerHTML = firstParagraph.innerHTML.slice(2);
+    }
 
-// append apostrophe if there is one after first letter
-if (firstParagraph.innerHTML[1] === "'") {
-  firstLetter += "'";
-  firstParagraph.innerHTML = firstParagraph.innerHTML.slice(2);
-}
+    // append apostrophe if there is one after first letter
+    if (firstParagraph.innerHTML[1] === "'") {
+      firstLetter += "'";
+      firstParagraph.innerHTML = firstParagraph.innerHTML.slice(2);
+    }
 
-firstParagraph.innerHTML = `<span class="drop-cap">${firstLetter}</span>${firstParagraph.innerHTML.slice(
-  1,
-)}`;
+    firstParagraph.innerHTML = `<span class="drop-cap">${firstLetter}</span>${firstParagraph.innerHTML.slice(
+      1,
+    )}`;
+  }
 }
