aac6fe9c1daa406a346a401236341b76b8817598
Abigaël <ablaldji@gmail.com>
2024-07-03 09:20:16 +0000

    editable pages

--- directus/extensions/directus-extension-tiptap/src/prosemirror-to-html/index.js
+++ directus/extensions/directus-extension-tiptap/src/prosemirror-to-html/index.js
@@ -90,6 +90,7 @@ export default ({ filter }) => {
     "content"
   );
   transformItemsFieldFromToHTML(filter, "Abstract.items.read", "content");
+  transformItemsFieldFromToHTML(filter, "Pages.items.read", "content");
 };
 
 function setLatestContentFromContentVersions(item) {
